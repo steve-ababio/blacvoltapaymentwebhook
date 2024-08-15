71f22f1b544b5d04012bb7c6c1d47b1f8fe71391
steve-ababio <stevo12424@live.com>
2024-08-15 06:02:56 +0000

    made some changes

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ require('dotenv').config();
 const express = require("express");
 const http = require("http");
 const {Payment} = require("./controllers/payment.js");
-const { prisma } = require('./lib/prisma.js');
 
 const app = express();
 const httpserver = http.createServer(app);
+app.use(express.json());
 app.post("/webhook/payment",Payment);
-app.get("/events",async function(req,res){
-    const results = await prisma.event.findMany({});
-    res.json({results});
-})
 httpserver.listen(process.env.PORT||8000, function () {
     console.log(`listening on port ${process.env.PORT}...`);
 });
--- controllers/payment.js
+++ controllers/payment.js
@@ -1,15 +1,17 @@
-// import { createHmac } from "crypto";
-// import { prisma } from "./lib/prisma.js";
 const {createHmac} = require("crypto");
+const { prisma } = require('../lib/prisma.js');
 
 async function Payment(req, res) {
     const hash = createHmac("sha512", process.env.SECRET).update(JSON.stringify(req.body)).digest('hex');
-    if (hash === req.headers['x-paystack-signature']) {
+    console.log(hash === req.headers['x-paystack-signature'])
+    if(hash === req.headers['x-paystack-signature']) {
         const event = req.body;
-        if (event && event.status === "success") {
+        const eventId = event.data.metadata.custom_fields[0].value;
+        console.log(event.event)
+        if(event && event.event.toString() === "charge.success") {
             await prisma.event.update({
                 where: {
-                    EventId: event.metadata.eventId
+                    EventId: eventId
                 },
                 data: {
                     paid: true
@@ -17,7 +19,6 @@ async function Payment(req, res) {
             });
         }
     }
-    res.send(200);
+    res.sendStatus(200);
 }
-
 module.exports = {Payment};
