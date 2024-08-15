cf69e684b3d89a1df5723f24059c47a39d78181d
steve-ababio <stevo12424@live.com>
2024-08-15 01:29:54 +0000

    added start command to package.json

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require("express");
 const http = require("http");
-const {Payment} = require("./controllers/payment.js")
+const {Payment} = require("./controllers/payment.js");
+const { prisma } = require('./lib/prisma.js');
 
 const app = express();
 const httpserver = http.createServer(app);
 app.post("/webhook/payment",Payment);
-
+app.get("/events",async function(req,res){
+    const results = await prisma.event.findMany({});
+    res.json({results});
+})
 httpserver.listen(process.env.PORT||8000, function () {
     console.log(`listening on port ${process.env.PORT}...`);
 });
