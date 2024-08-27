e62e4738444f0be295a7df4ce22cd769200d4525
steve-ababio <stevo12424@live.com>
2024-08-27 01:36:09 +0000

    added print options

--- controllers/payment.js
+++ controllers/payment.js
@@ -3,20 +3,24 @@ const { prisma } = require('../lib/prisma.js');
 
 async function Payment(req, res) {
     const hash = createHmac("sha512", process.env.SECRET).update(JSON.stringify(req.body)).digest('hex');
-    console.log(hash === req.headers['x-paystack-signature'])
     if(hash === req.headers['x-paystack-signature']) {
         const event = req.body;
         const eventId = event.data.metadata.custom_fields[0].value;
-        console.log(event.event)
+        console.log("Event Id: ",eventId);
         if(event && event.event.toString() === "charge.success") {
-            await prisma.event.update({
-                where: {
-                    EventId: eventId
-                },
-                data: {
-                    paid: true
-                }
-            });
+            try{
+                const result = await prisma.event.update({
+                    where: {
+                        EventId: eventId
+                    },
+                    data: {
+                        paid: true
+                    }
+                });
+                console.log(result);
+            }catch(error){
+                console.log(error);
+            }
         }
     }
     res.sendStatus(200);
