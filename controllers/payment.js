995dd630b5becb68660f8c3dec001fe76607f3c5
steve-ababio <stevo12424@live.com>
2024-08-29 13:27:39 +0000

    Changed payment link

--- controllers/payment.js
+++ controllers/payment.js
@@ -6,7 +6,7 @@ async function Payment(req, res) {
     if(hash === req.headers['x-paystack-signature']) {
         const event = req.body;
         const eventId = event.data.metadata.custom_fields[0].value;
-        console.log("Event Id: ",eventId);
+        console.log("Event Id: ",event.data.metadata);
         if(event && event.event.toString() === "charge.success") {
             try{
                 const result = await prisma.event.update({
